50857b1068b70eaf8b949282e230780eaaea254e
p20224-crypto <p20224@sw.hs.kr>
2025-10-29 18:36:25 +0900

    Update quiz.py

--- pages/quiz.py
+++ pages/quiz.py
@@ -5,7 +5,7 @@ st.markdown("""
 @keyframes gradientMove {
     0% { background-position: 0% 50%; }
     50% { background-position: 100% 50%; }
-    100% { background-position: 0% 50%; } #배경화면 애니메이션
+    100% { background-position: 0% 50%; }
 }
 
 [data-testid="stAppViewContainer"] {
@@ -13,7 +13,7 @@ st.markdown("""
     background-size: 300% 300%;
     animation: gradientMove 12s ease infinite;
     color: white;
-    transition: all 0.5s ease; #메인 배경화면
+    transition: all 0.5s ease;
 }
 
 [data-testid="stSidebar"] {
@@ -21,20 +21,20 @@ st.markdown("""
     background-size: 300% 300%;
     animation: gradientMove 12s ease infinite;
     color: #f8faff;
-    transition: all 0.5s ease; #왼쪽 사이드바
+    transition: all 0.5s ease;
 }
 
 .stApp h1 {
     color: #dbeafe;
     text-shadow: 2px 2px 5px #000;
-    font-weight: 900; #제목
+    font-weight: 900;
     text-shadow: none;
 }
 
 .stApp h3 {
     color: #0f172a;
     text-shadow: 1px 1px 4px #000;
-    font-weight: 800; #문제 제목 색
+    font-weight: 800;
     text-shadow: none;
 }
 
@@ -42,7 +42,7 @@ st.markdown("""
     color: #4b5563;
     font-size: 1.1em;
     text-shadow: 0px 0px 3px rgba(0,0,0,0.5);
-    font-weight: 500; #문제 내용 색
+    font-weight: 500;
     text-shadow: none;
 }
 
@@ -55,13 +55,13 @@ div.stButton > button {
     font-weight: bold;
     border: none;
     box-shadow: 0px 0px 10px rgba(102,166,255,0.5);
-    transition: all 0.3s ease; #버튼
+    transition: all 0.3s ease;
 }
 
 div.stButton > button:hover {
     background: linear-gradient(90deg, #66a6ff, #89f7fe);
     box-shadow: 0px 0px 20px rgba(137,247,254,0.8);
-    transform: scale(1.05); #버튼 올렸을때
+    transform: scale(1.05);
 </style>
 """, unsafe_allow_html=True)
 
